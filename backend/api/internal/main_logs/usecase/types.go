382e39f343df891d4cb1221743b43a23fec367dd
371_Methat <mathelos8989@gmail.com>
2026-07-20 09:48:39 +0700

    fix/api-sections

--- backend/api/internal/main_logs/usecase/types.go
+++ backend/api/internal/main_logs/usecase/types.go
@@ -3,21 +3,25 @@ package usecase
 import "omnilogs-api/internal/main_logs/document"
 
 type SearchInput struct {
-	ActorUserID   uint
-	PlatformAdmin bool
-	ProductID     int64
-	EnvironmentID *int64
-	ProjectID     *int64
-	ProjectIDs    []int64
-	CategoryID    *int64
-	CategoryIDs   []int64
-	Level         *string
-	LogType       *string
-	RequestID     *string
-	TraceID       *string
-	Keyword       *string
-	Page          int
-	PerPage       int
+	ActorUserID      uint
+	PlatformAdmin    bool
+	ProductID        int64
+	EnvironmentID    *int64
+	ProjectID        *int64
+	ProjectIDs       []int64
+	CategoryID       *int64
+	CategoryIDs      []int64
+	Level            *string
+	LogType          *string
+	RequestID        *string
+	TraceID          *string
+	CustomFieldPath  *string
+	CustomFieldValue *string
+	SortField        *string
+	SortOrder        string
+	Keyword          *string
+	Page             int
+	PerPage          int
 }
 
 type MainLogDocument = document.MainLog
